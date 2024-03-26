6a385998c00cce8e40d8b48844c3e8622f4de42a
meddd1 <164946466+meddd1@users.noreply.github.com>
2024-03-26 16:42:38 +0000

    ml_backend.py

--- ml_backend.py
+++ ml_backend.py
@@ -2,7 +2,7 @@ import openai
 
 class ml_backend:
         
-    openai.api_key = 'YOUR-API-KEY-HERE'
+    openai.api_key = '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'
 
     def generate_email(self, userPrompt ="Write me a professionally sounding email", start="Dear"):
         """Returns a generated an email using GPT3 with a certain prompt and starting sentence"""
